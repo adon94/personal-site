fa1404a18f350033d41c0f8ed14dc7603112ef0d
adon94 <adambfad@gmail.com>
2022-08-15 13:42:56 +0100

    fix footer link

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,24 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Created by Adam O&apos;Neill.
-        </a>
+        <p>
+          Created on&nbsp;
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noreferrer"
+            className="next-link"
+          >
+            Next.js
+          </a>
+          &nbsp;by Adam O&apos;Neill.
+        </p>
       </footer>
+      <style jsx>{`
+        .next-link {
+          display: inline-block;
+        }
+      `}</style>
     </div>
   )
 }
