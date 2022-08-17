d078d2184f561518ac3479262208c74bad3f8a54
adon94 <adambfad@gmail.com>
2022-08-17 15:31:08 +0100

    add analytics

--- components/whatDo.js
+++ components/whatDo.js
@@ -1,9 +1,14 @@
+import { useEffect, useState } from 'react';
 import { isMobile } from 'react-device-detect';
 
 export default function WhatDo() {
+  const [scrollSpeed, setScrollSpeed] = useState(0);
+  useEffect(() => {
+    if (!isMobile) setScrollSpeed(6);
+  }, [])
   return (
     <div className="container h-screen flex justify-center items-center">
-      <p data-scroll data-scroll-speed={isMobile ? 0 : 6} className="text-5xl md:text-9xl text-left my-5">
+      <p data-scroll data-scroll-speed={scrollSpeed} className="text-5xl md:text-9xl text-left my-5">
         <span>I support</span>
         <br></br>
         <span>designers and</span>
--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,52 @@
-import { useRef } from 'react'
-import { useRouter } from 'next/router'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-import 'locomotive-scroll/dist/locomotive-scroll.css';
-import '../styles/globals.css'
+import { useRef } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const { asPath } = useRouter();
-  const containerRef = useRef(null)
+  const containerRef = useRef(null);
   return (
-    <LocomotiveScrollProvider
-      options={
-        {
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="0">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+          `}
+      </Script>
+      <LocomotiveScrollProvider
+        options={{
           smooth: true,
-          // ... all available Locomotive Scroll instance options 
+          // ... all available Locomotive Scroll instance options
+        }}
+        watch={
+          [
+            //..all the dependencies you want to watch to update the scroll.
+            //  Basicaly, you would want to watch page/location changes
+            //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+          ]
         }
-      }
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      location={asPath}
-      onLocationChange={(scroll) => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
-      containerRef={containerRef}
-    >
-      <div data-scroll-container ref={containerRef}>
-        <Component {...pageProps} />
-      </div>
-    </LocomotiveScrollProvider>
-  )
+        location={asPath}
+        onLocationChange={(scroll) =>
+          scroll.scrollTo(0, { duration: 0, disableLerp: true })
+        }
+        containerRef={containerRef}
+      >
+        <div data-scroll-container ref={containerRef}>
+          <Component {...pageProps} />
+        </div>
+      </LocomotiveScrollProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
